4d8ee80440ca601a210543deff210cf248bb6dd9
Mizba <mizba6052@gmail.com>
2026-07-05 22:35:35 +0530

    Fix Renderport

--- app.py
+++ app.py
@@ -44,6 +44,9 @@ def predict():
         image=filepath,
         caption=caption
     )
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
